5d50f157403e6bf1944c9249d03954087bc7c7ca
speculatecat <speculate_cat@163.com>
2017-03-23 22:49:58 +0800

    Add comment.

--- Base/UnionFind/UnionFind.py
+++ Base/UnionFind/UnionFind.py
@@ -3,6 +3,11 @@
 # email:yan@wosdata.com
 
 
+# Example
+# 0   1 - 2   3 - 4
+# |       |   |   |
+# 5 - 6   7   8   9
+
 class UnionFind(object):
     def __init__(self, n):
         self.count = n
